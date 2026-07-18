3a7d9b6f580c15c177da5445d37e2eef43b56ecb
Boopathy N Ramachandran <boopathy.nr@gmail.com>
2026-07-18 14:58:46 +0530

    Initial Commit

---
+++ Doppio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Doppio
+//
+//  Created by Boopathy N R on 18/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Doppio/DoppioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoppioApp.swift
+//  Doppio
+//
+//  Created by Boopathy N R on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct DoppioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
